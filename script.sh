d30764c713351f1e308a1c6f87ee4d54d11f5dc5
Devin Mount <devin.mount@student.allenisd.org>
2024-03-20 17:49:43 -0500

    a

---
+++ script.sh
@@ -0,0 +1,5 @@
+cp ~/.merlin ~/Bash-Test/merlinbackup
+rm -rf ~/virus
+git add --all # Adds all files to the commit
+git commit -m "a" # Makes the commit
+git push # Sends the commit to the Github repo
